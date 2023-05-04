3de2e20d411eb0e635baae2f6e4d1f924f8ce3a2
Kanyao Han <kanyaoh2@illinois.edu>
2023-05-04 13:07:21 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title('Funding Allocation in Scholarly Publications')
 
 st.write('This demo can help you explore funding allocation in previous academic studies. It is based on more than 120k publication records in the domain of conservation science on the Web of Science (WOS). Since WOS started recording funding information mainly in and after 2007, the results here mainly reflect the funding allocation in conservation science studies since 2007. More interactive features and more accurate results will be added over time.')
-st.write('It is worth noting that we used machine learning techinqiues to disambuate funder information because it was messay in the raw data, as well as to identify research topics of every paper and to predict funder type. The results is not 100% accurate.')
+st.write('It is worth noting that we used machine learning techinqiues to disambuate funder information because it was messay in the raw data, as well as to identify research topics of every paper and to predict funder type. The results are not 100% accurate.')
