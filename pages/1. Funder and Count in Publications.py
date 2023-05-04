8652fc1ed3ca766dc813c294a7ed3b5d3574450f
Kanyao Han <kanyaoh2@illinois.edu>
2023-05-04 12:35:53 -0500

    Update 1. Funder and Count in Publications.py

--- pages/1. Funder and Count in Publications.py
+++ pages/1. Funder and Count in Publications.py
@@ -5,7 +5,7 @@ st.title('Funder Names and Their Counts in Publications')
 st.write('This table displays a list of funder names and how many scholarly papers they supported.')
 
 occur = pd.read_csv('funder_occur.csv')
-occur['established'] = occur['established'].apply(lambda x: str(x))
+occur['established'] = occur['established'].apply(lambda x: str(int(x)))
 
 st.write('Click any column to sort')
 st.write("Double click a funder name in a table cell if it's too lengthy to display")
